e80e24aabdc72921af09db14380e7de42d6c823a
krab <cool.krab9@yandex.ru>
2019-04-27 18:52:41 +0300

    Southern.py ready

---
+++ first_les/southern.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+
+cities = sys.argv[1:]
+
+max_coords = 0
+southern_city = ''
+response = None
+
+for i in cities:
+    try:
+        response = requests.get(f'http://geocode-maps.yandex.ru/1.x/?geocode={i}&format=json')
+    except Exception as e:
+        print('Не удается подключиться к сети Интернет, попробуйте еще раз')
+        sys.exit()
+
+    if response:
+        json_response = response.json()
+        south = float(
+            json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
+                'Point'][
+                'pos'][1])
+        if float(south) > max_coords:
+            southern_city = i
+            max_coords = south
+    else:
+        print('Что-то пошло не так, попробуйте позже')
+print(southern_city)
